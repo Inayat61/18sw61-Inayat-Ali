603961a539011836c513c604aa5f100274872bbc
Inayat61 <49411975+Inayat61@users.noreply.github.com>
2019-04-24 14:26:34 +0500

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,21 @@
+class Even
+{
+	public static void main(String []arg)
+	{
+		for(int i=0;i<=10;i++)
+		{
+			if(i%2!=0)
+			{continue;}
+		System.out.print(i);
+		
+		}System.out.println();
+		
+		for(int i=0;i<=10;i++)
+		{
+			if(i%2==0)
+			{continue;}
+		System.out.print(i);
+		
+		}
+	}
+}
